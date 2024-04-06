596db6226117dc37d5fd2c52a171751e31c4a551
Сорок два <sorokdva.developer@gmail.com>
2024-04-06 19:28:50 +0200

    feat: mat-icon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+	constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+		this.matIconRegistry.addSvgIcon('ripeurs-logo', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/svg/ripeurs-logo.svg'));
+	}
 
   ngOnInit(): void {
   }
